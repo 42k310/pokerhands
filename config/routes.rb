6ae1ef44f8e6bf381bfad1c26dfa1e151b2f69f8
keng310 <ksapr02@gmail.com>
2021-10-14 19:12:42 +0900

    routingにコメント入れただけ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # web
   root to: 'top#index'
   get 'top/judge' => 'top#judge'
-  post 'top/judge' => 'topjudge'
+  post 'top/judge' => 'top#judge'
 
+  # web
   mount BASE::API => '/'
 end
